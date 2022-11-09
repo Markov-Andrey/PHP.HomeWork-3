9c73bcde2aa3daa2c5504e450920147e71cb9984
Markov Andrey <andremarkov@icloud.com>
2022-11-09 13:51:10 +0300

    start work

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $htmlTitle = 'HomeWork PHP#2';
+  include('./src/header.php');
+  include('./src/function.php');
+
+  //функция автодобавления Echo со вложенным тегом <p>
+  //function add($text)
+  
+  //функция добавляющее заголовок "условие" со вложенным текстом
+  //condition($text)
+  
+  //функция добавления начала и конца DIVа с авто-заголовком по запросу
+  //task($arg), $arg = "start" или "end"
+
+  
+
+  include('./src/footer.php');
+?>
